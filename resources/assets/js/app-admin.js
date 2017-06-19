dfacadddfc710694f1ee10eb262a0b439e713b15
van <Demarrer2017>
2017-06-19 21:43:06 +0200

    Home page desktop done

--- resources/assets/js/app-admin.js
+++ resources/assets/js/app-admin.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    Dropzone.options.formupload2 = {
+    maxFiles: 1,
+    accept: function(file, done) {
+        console.log(file);
+        done();
+    },
+    init: function() {
+        this.on("maxfilesexceeded", function(file){
+            alert("No more files please!");
+        });
+    }
+};
 
 });
 
